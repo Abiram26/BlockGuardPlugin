e5cc2d51b2e1d82bddc729c9123e3f7cf1fac5fe
Abiram <abiram26@hotmail.com>
2014-05-24 17:31:40 +0200

    Create Information.java

---
+++ com/abiram26/blockguard/commands/Information.java
@@ -0,0 +1,23 @@
+package com.abiram26.blockguard.commands;
+
+import com.abiram26.blockguard.Plugin;
+import com.mbserver.api.CommandExecutor;
+import com.mbserver.api.CommandSender;
+
+public class Information implements CommandExecutor {
+	
+	@Override
+	public void execute(String command, CommandSender sender, String[] args,
+			String label) {
+		// TODO Auto-generated method stub
+		sender.sendMessage(Plugin.stamp+"All commands:");
+		sender.sendMessage("/bg [Shows all commands]");
+		sender.sendMessage("/bgpos1 [Marks position 1, so a region can be set]");
+		sender.sendMessage("/bgpos2 [Marks position 2, so a region can be set]");
+		sender.sendMessage("/bgset <regionname> [Creates a region with you as the owner]");
+		sender.sendMessage("/bgaddmember <regionname> <membername> [Adds a member to a region]");
+		sender.sendMessage("/bgremovemember <regionname> <membername> [Removes a member from a region]");
+		sender.sendMessage("/removeregion <regionname> [Removes a region (and it's members)]");
+	}
+
+}
