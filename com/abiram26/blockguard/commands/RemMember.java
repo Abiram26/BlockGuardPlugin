09c3359ab2ef655409b99224e3b5dfab01c21767
Abiram <abiram26@hotmail.com>
2014-06-27 12:46:28 +0200

    Update RemMember.java

--- com/abiram26/blockguard/commands/RemMember.java
+++ com/abiram26/blockguard/commands/RemMember.java
@@ -1,49 +1,58 @@
 package com.abiram26.blockguard.commands;
 
-import com.abiram26.blockguard.Config;
 import com.abiram26.blockguard.Plugin;
+import com.abiram26.blockguard.storage.Config;
 import com.mbserver.api.CommandExecutor;
 import com.mbserver.api.CommandSender;
 
 public class RemMember implements CommandExecutor {
-		private final Plugin plugin;
-		private final Config getConfig;
-	public RemMember(Plugin plugin) {
+	private final Plugin plugin;
+
+	public RemMember(final Plugin plugin) {
 		// TODO Auto-generated constructor stub
-		this.plugin=plugin;
-		this.getConfig=this.plugin.getConfig();
+		this.plugin = plugin;;
 	}
 
 	@Override
-	public void execute(String command, CommandSender sender, String[] args,
-			String label) {
+	public void execute(final String command, final CommandSender sender,
+			final String[] args, final String label) {
 		// TODO Auto-generated method stub
 		final int regionId;
 		final int memberId;
 		if (sender.hasPermission("abiram26.blockguard.*")) {
-
 			if (args.length == 2) {
-				regionId = this.getConfig.hasRegion(args[0].toLowerCase());
+				final Config cfg = this.plugin.getConfig();
+				regionId = cfg.hasRegion(args[0]);
 				if (regionId != -1) {
-					memberId=this.getConfig.getMembers().get(regionId)
-							.hasMember(args[1].toLowerCase());
-					if (memberId!=-1) {
-						this.getConfig.getMembers().get(regionId).getMemberList().remove(memberId);
+					memberId = cfg.getMembers().get(regionId)
+							.hasMember(args[1]);
+					if (memberId != -1) {
+						cfg.getMembers().get(regionId).getMemberList()
+								.remove(memberId);
 						this.plugin.saveConfig();
-						sender.sendMessage(Plugin.stamp + "Removed member " + args[1].toLowerCase() + " from " + this.getConfig.getRegions().get(regionId).getRegionName()+"!");
+						sender.sendMessage(Plugin.stamp
+								+ "Removed member "
+								+ args[1]
+								+ " from "
+								+ cfg.getRegions().get(regionId)
+										.getRegionName() + "!");
 					} else {
-						sender.sendMessage(Plugin.stamp + "Not removed: The region does not have that name as a member!");
+						sender.sendMessage(Plugin.stamp
+								+ "Not removed: The region does not have that name as a member!");
 					}
 				} else {
-					sender.sendMessage(Plugin.stamp + "Not removed: No region with that name!");
+					sender.sendMessage(Plugin.stamp
+							+ "Not removed: No region with that name!");
 				}
 			} else {
-				sender.sendMessage(Plugin.stamp + "Not removed: Not enough arguments given, usage: /bgremovemember <regionname> <playername>");
+				sender.sendMessage(Plugin.stamp
+						+ "Not removed: Not enough arguments given, usage: /bgremovemember <regionname> <playername>");
 			}
 		} else {
-			sender.sendMessage(Plugin.stamp + "Not removed: No permission to use this command!");
+			sender.sendMessage(Plugin.stamp
+					+ "Not removed: No permission to use this command!");
 		}
-		
+
 	}
 
 }
