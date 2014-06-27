99003808e9578f89c38c615818212e7bfd335c17
Abiram <abiram26@hotmail.com>
2014-06-27 12:49:38 +0200

    Update BlockEventHandler.java
    added final modifiers

--- com/abiram26/blockguard/events/BlockEventHandler.java
+++ com/abiram26/blockguard/events/BlockEventHandler.java
@@ -1,7 +1,7 @@
 package com.abiram26.blockguard.events;
 
-import com.abiram26.blockguard.Config;
 import com.abiram26.blockguard.Plugin;
+import com.abiram26.blockguard.storage.Config;
 import com.mbserver.api.events.BlockEvent;
 import com.mbserver.api.events.EventHandler;
 import com.mbserver.api.events.Listener;
@@ -9,16 +9,16 @@ import com.mbserver.api.events.Listener;
 public class BlockEventHandler implements Listener {
 	private Config config;
 
-	public BlockEventHandler(Config config) {
+	public BlockEventHandler(final Config config) {
 		// TODO Auto-generated constructor stub
 		this.config = config;
 	}
 
 	@EventHandler
-	public void onBlockAction(BlockEvent event) {
-		int blockX = event.getLocation().getBlockX();
-		int blockY = event.getLocation().getBlockY();
-		int blockZ = event.getLocation().getBlockZ();
+	public void onBlockAction(final BlockEvent event) {
+		final int blockX = event.getLocation().getBlockX();
+		final int blockY = event.getLocation().getBlockY();
+		final int blockZ = event.getLocation().getBlockZ();
 		if (!(event.getPlayer().hasPermission("abiram26.blockguard.*"))) {
 			for (int x = 0; x < this.config.getRegions().size(); x++) {
 				if (this.config.getRegions().get(x)
@@ -27,7 +27,7 @@ public class BlockEventHandler implements Listener {
 							.getMembers()
 							.get(x)
 							.hasMember(
-									event.getPlayer().getName().toLowerCase()) == -1) {
+									event.getPlayer().getName()) == -1) {
 						event.getPlayer().sendMessage(
 								Plugin.stamp + "Region "
 										+ this.config.getRegions().get(x)
