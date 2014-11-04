67487b834d7143e48433c6893ec22378708b62b3
Abiram <abiram26@hotmail.com>
2014-11-04 16:35:29 +0100

    pre-v2.2

--- com/abiram26/blockguard/commands/RegionList.java
+++ com/abiram26/blockguard/commands/RegionList.java
@@ -1,28 +1,21 @@
 package com.abiram26.blockguard.commands;
 
-import com.abiram26.blockguard.Plugin;
-import com.abiram26.blockguard.storage.Config;
+import com.abiram26.blockguard.BlockGuardPlugin;
 import com.mbserver.api.CommandExecutor;
 import com.mbserver.api.CommandSender;
 
 public class RegionList implements CommandExecutor {
-	private final Plugin p1;
+	private final BlockGuardPlugin p1;
 
-	public RegionList(final Plugin plugin) {
+	public RegionList(final BlockGuardPlugin plugin) {
 		// TODO Auto-generated constructor stub
 		this.p1 = plugin;
 	}
 
 	@Override
-	public void execute(final String command, final CommandSender sender, final String[] args,
-			final String label) {
-		// TODO Auto-generated method stub
-		final Config cfg = p1.getConfig();
-		final int amountOfRegions = cfg.getRegions().size();
-		sender.sendMessage(Plugin.stamp+"All regions with their owner:");
-		for (int x = 0; amountOfRegions > x; x++) {
-			sender.sendMessage("Region: " + cfg.getRegions().get(x).getRegionName() + " with owner: " + cfg.getMembers().get(x).getMemberList().get(0));
-		}
+	public void execute(final String command, final CommandSender sender,
+			final String[] args, final String label) {
+		p1.getServer().executeCommand(sender, "bgra", new String[]{"regions"});
 	}
 
 }
