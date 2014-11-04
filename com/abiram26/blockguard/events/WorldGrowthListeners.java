837af5568bbe6cb57810b72e7f9226ce384f2bad
Abiram <abiram26@hotmail.com>
2014-11-04 17:02:04 +0100

    pre-v2.2

---
+++ com/abiram26/blockguard/events/WorldGrowthListeners.java
@@ -0,0 +1,28 @@
+package com.abiram26.blockguard.events;
+
+import com.abiram26.blockguard.storage.RegionFlags;
+import com.abiram26.blockguard.storage.RegionMembers;
+import com.abiram26.blockguard.storage.Regions;
+import com.mbserver.api.events.Listener;
+
+public class WorldGrowthListeners implements Listener {
+
+	//private final Regions cfg1;
+	//private final RegionMembers cfg2;
+	//private final RegionFlags cfg3;
+
+	public WorldGrowthListeners(final Regions regions,
+			final RegionMembers members, final RegionFlags flags) {
+	//	this.cfg1 = regions;
+	//	this.cfg2 = members;
+	//	this.cfg3 = flags;
+	}
+	
+//this class does nothing yet...
+//TODO:
+//Add eventlisteners for these flags:
+//  can_grass_grow -> normal grass
+//  can_plant_grow -> crops and agricultural stuff
+//  can_tree_grow -> tree's only
+
+}
